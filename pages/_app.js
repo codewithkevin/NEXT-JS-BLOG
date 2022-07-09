d75566dcd9237f2e4f35d034fcd31781837450f0
codewithkevin <oseikelvin709@gmail.com>
2022-07-09 15:15:59 +0000

    sitting up scss

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/globals.css'
+import '../styles/global.scss'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import {PostCard, PostWidget, Categories} from '../components/index'
 
 
 export default function Home() {
@@ -18,17 +19,14 @@ export default function Home() {
 
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
         <div className='lg:col-span-8 col-span-1'>
-          {post.map((post, index) => (
-            // eslint-disable-next-line react/jsx-key
-            <div>
-              {post.title}
-              {post.excerpt}
-            </div>  
+          {post.map((post) => (
+            <PostCard post={post} key={post.title} />
           ))}
         </div>
         <div className="lg:col-span-4 col-span-1">
             <div className="lg:sticky relative top-8">
-                
+               <PostWidget />
+               <Categories />
             </div>
         </div>
       </div>
