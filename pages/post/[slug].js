a7b8457df5b8f14dc587a91a3316e0cf9921344a
codewithkevin <oseikelvin709@gmail.com>
2022-07-11 22:17:30 +0000

    sitting up components to be used in slug

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -6,7 +6,18 @@ const PostDetails = () => {
   return (
     <div className='container mx-auto px-10 mb-8'>
         <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
-            
+            <div className='col-span-1 lg:col-span-8'>
+                <PostDetail />
+                <Author />
+                <CommentForm />
+                <Comments />
+            </div>
+            <div className='col-span-1 lg:col-span-4'>
+                <div className='relative lg:sticky top-8'>
+                    <PostWidget />
+                    <Categories /> 
+                </div>
+            </div>
         </div>
     </div>
   )
