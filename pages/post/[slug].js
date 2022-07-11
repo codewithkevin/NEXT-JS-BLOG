c2336ba4d1196ebe256f7c535d58bc5e6839ff74
codewithkevin <oseikelvin709@gmail.com>
2022-07-11 20:42:23 +0000

    update

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,11 +1,13 @@
 import React from 'react'
 import {getPosts, getPostDeatails} from '../../services'
-import {PostDetails, Categories, PostWidget, Author, Comments, CommentForm} from '../../services'
+import {PostDetail, Categories, PostWidget, Author, Comments, CommentForm} from '../../services'
 
 const PostDetails = () => {
   return (
-    <div>
-
+    <div className='container mx-auto px-10 mb-8'>
+        <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
+            
+        </div>
     </div>
   )
 }
