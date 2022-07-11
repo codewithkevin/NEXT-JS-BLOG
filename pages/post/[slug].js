246821b34263e11a530bdcdcff00303d6e34e288
codewithkevin <oseikelvin709@gmail.com>
2022-07-11 20:31:53 +0000

    sitting up slug componenet

---
+++ pages/post/[slug].js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {getPosts, getPostDeatails} from '../../services'
+
+const PostDetails = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default PostDetails
