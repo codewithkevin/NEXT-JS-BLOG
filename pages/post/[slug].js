b95e4da09f25813d4a76184031a54307f4946ed5
codewithkevin <oseikelvin709@gmail.com>
2022-07-11 20:38:57 +0000

    update

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {getPosts, getPostDeatails} from '../../services'
+import {PostDetails, Categories, PostWidget, Author, Comments, CommentForm} from '../../services'
 
 const PostDetails = () => {
   return (
