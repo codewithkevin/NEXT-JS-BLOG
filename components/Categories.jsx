5ecc71ccdf14decd3e990b8a48512bd4815d9b30
codewithkevin <oseikelvin709@gmail.com>
2022-07-11 06:22:02 +0000

    update

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -1,7 +1,14 @@
 import React, {useState, useEffect} from 'react'
 import Link from 'next/link'
+import {getCategories} from '../services'
 
 const Categories = () => {
+  const [category, setCategory] = useState([]);
+
+  useEffect(() =>{
+      getCategories().then((newCategories) => setCategory(newCategories))
+  }, [])
+  
   return (
     <div>
       Categories
