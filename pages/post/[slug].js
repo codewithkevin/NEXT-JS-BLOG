e7c35ac81bbad13a1dc70352459ea1b11dddfbf8
codewithkevin <oseikelvin709@gmail.com>
2022-07-12 07:27:39 +0000

    update

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -3,15 +3,15 @@ import {getPosts, getPostDetails} from '../../services'
 import {PostDetail, Categories, PostWidget, Author, Comments, CommentForm} from '../../components'
 
 const PostDetails = ({post}) => {
-    console.log(post)
+
   return (
     <div className='container mx-auto px-10 mb-8'>
         <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
             <div className='col-span-1 lg:col-span-8'>
-                <PostDetail />
-                <Author />
-                <CommentForm />
-                <Comments />
+                <PostDetail post={post}/>
+                <Author post={post.author} />
+                <CommentForm slug={post.slug} />
+                <Comments slug={post.slug}/>
             </div>
             <div className='col-span-1 lg:col-span-4'>
                 <div className='relative lg:sticky top-8'>
