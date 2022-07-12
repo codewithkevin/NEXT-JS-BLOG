5d64afc6827cdfad93b391cdd4c03f079fde19bb
codewithkevin <oseikelvin709@gmail.com>
2022-07-12 08:53:26 +0000

    update

--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -1,9 +1,17 @@
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable jsx-a11y/alt-text */
 import React from 'react'
 
-const PostDetail = () => {
+const PostDetail = ({post}) => {
   return (
-    <div>
-      
+    <div className="bg-white shadow-lg rounded-lg  lg:p-8 pb-12 mb-8">
+        <div className="relative overflow-hidden shadow-md mb-6">
+            <img 
+              src={post.featuredImage.url}
+              alt={post.title}
+              className="w-full h-1/5	"
+            />
+        </div>
     </div>
   )
 }
