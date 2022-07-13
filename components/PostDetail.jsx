abff759d478a548e64abc98ca4e7231345dadc2f
codewithkevin <oseikelvin709@gmail.com>
2022-07-13 11:25:05 +0000

    update

--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -2,6 +2,7 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React from 'react'
 import moment from 'moment'
+import Link from 'next/link'
 
 
 const PostDetail = ({post}) => {
@@ -11,19 +12,31 @@ const PostDetail = ({post}) => {
             <img 
               src={post.featuredImage.url}
               alt={post.title}
-              className="object-top h-[30%] w-[50%]  rounded-t-lg"
+              className="object-top w-full h-full  rounded-t-lg"
             />
         </div>
         <div className="px-4 lg:px-8">
-            <div className="flex items-center mb-8 w-full">
-              <div className="w-16 flex-none">
-                  <img 
-                  src={post.featuredImage.url} 
-                  alt={post.title}
-                  className="align-middle rounded-full h-[60px] w-[60px]" />
+          <div className="flex items-center mb-8 w-full">
+              <div className="hidden md:flex items-center justify-center lg:mb-0 lg:w-auto mr-8 items-center">
+                <img
+                  alt={post.author.name}
+                  height="30px"
+                  width="30px"
+                  className="align-middle rounded-full"
+                  src={post.author.photo.url}
+                />
+                <p className="inline align-middle text-gray-700 ml-2 font-medium text-lg">{post.author.name}</p>
               </div>
-            </div>
+              <div className="font-medium text-gray-700">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline mr-2 text-blue-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+                </svg>
+                <span className="align-middle">{moment(post.createdAt).format('MMM DD, YYYY')}</span>
+              </div>
+          </div>
         </div>
+        <h1 className="mb-8 text-3xl text-black ml-4 font-bold">{post.title}</h1>
+        <p>{post.description}</p>
     </div>
   )
 }
