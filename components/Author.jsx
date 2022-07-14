24718f74e4333c846161f552a2b9dd88a3eaa696
codewithkevin <oseikelvin709@gmail.com>
2022-07-14 13:37:10 +0000

    done with author

--- components/Author.jsx
+++ components/Author.jsx
@@ -1,14 +1,23 @@
-/* eslint-disable @next/next/no-img-element */
 import React from 'react'
+import Image from 'next/image';
 
 const Author = ({author}) => {
   return (
-    <div>
-      <img 
-        alt={author.name}
-        src={author.photo.url}
-        className="align-middle rounded-full h-[100px] w-[100px]"
-      />
+    <div className='text-center mt-20 mb-8 p-12 rounded-lg bg-black bg-opacity-20'>
+      <div className='author left-0 right-0 -top-14'>
+        <div className='flex'>
+            <Image 
+              alt={author.name}
+              unoptimized
+              src={author.photo.url}
+              height="100px"
+              width="100px"
+              className="align-middle rounded-full"
+            />
+            <h3 className="text-white ml-10 my-7 text-xl font-bold">{author.name}</h3>
+            </div>
+        <p className='mt-5 text-lg'>{author.bio}</p>
+      </div>
     </div>
   )
 }
