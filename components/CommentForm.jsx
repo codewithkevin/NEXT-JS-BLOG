90bc38d58d348fbc01e5b44bd92553ec41ec0e79
codewithkevin <oseikelvin709@gmail.com>
2022-07-14 18:06:39 +0000

    update

--- components/CommentForm.jsx
+++ components/CommentForm.jsx
@@ -18,19 +18,30 @@ const CommentForm = () => {
       <div className="grid grid-cols-1 gap-4 mb-4">
             <textarea 
             ref={commentEl} 
-            className="p-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray bg-gray-100 text-gray-700"
-            placeholder='comment'
+            className="p-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray bg-gray-100 text-black"
+            placeholder='Comment'
             name="comment"
             />
       </div>
 
-      <div className="grid grid-cols-1 gap-4 mb-4">
-
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 mb-4">
+          <input 
+            type="text"
+            placeholder="Name"
+            name="name"
+            className="py-2 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray bg-gray-100 text-black"
+            ref={nameEl}
+          />
+          <input 
+            type="text"
+            placeholder="Email"
+            name="email"
+            className="py-2 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray bg-gray-100 text-black"
+            ref={emailEl}
+          />
       </div>
 
-      <div className="grid grid-cols-1 gap-4 mb-4">
-
-      </div>
+      {error && <p className="text-xs text-red-500">All fields are required</p>}
 
     </div>
   )
