a2f31764b0a061d980c26e02d7d4aa9b62d6779d
codewithkevin <oseikelvin709@gmail.com>
2022-07-14 11:17:41 +0000

    update

--- components/Author.jsx
+++ components/Author.jsx
@@ -1,8 +1,15 @@
+/* eslint-disable @next/next/no-img-element */
 import React from 'react'
 
-const Author = () => {
+const Author = ({author}) => {
   return (
-    <div>Author</div>
+    <div>
+      <img 
+        alt={author.name}
+        src={author.photo.url}
+        className="align-middle rounded-full h-[100px] w-[100px]"
+      />
+    </div>
   )
 }
 
--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -9,7 +9,7 @@ const PostDetails = ({post}) => {
         <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
             <div className='col-span-1 lg:col-span-8'>
                 <PostDetail post={post}/>
-                <Author post={post.author} />
+                <Author author={post.author} />
                 <CommentForm slug={post.slug} />
                 <Comments slug={post.slug}/>
             </div>
