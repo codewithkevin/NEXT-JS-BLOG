3b2e72092cfeae163df43fd93b94a321848fdd44
codewithkevin <oseikelvin709@gmail.com>
2022-07-15 10:32:24 +0000

    update

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,23 +1,24 @@
 import React from 'react'
 // index.js
+import { FaMobileAlt } from "react-icons/fa";
+import { FaLaptopCode } from "react-icons/fa";
+import { BiWebcam } from "react-icons/bi";
 
-// Import the FontAwesomeIcon component
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
-// import the icons you need
-import {
-  faSearch,
-  faAmbulance,
-  faAnchor,
-} from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
   return (
-    <div className="bg-white text-black h-[20%] w-14">
-        <FontAwesomeIcon
-        icon={faAnchor}
-        className="text-xs text-red-800"
-      />
+    <div className="bg-black text-white h-[15%] w-14 mt-10 rounded-lg">
+          <div className="text-center justify-space p-3 py-2  mt-20 cursor-pointer">
+             <div className="text-[30px] mb-3 icons">
+                <FaMobileAlt />
+             </div>
+             <div className="text-[30px] mb-3 icons">
+                <BiWebcam />
+             </div>
+             <div className="text-[30px] mb-3 icons">
+                <FaLaptopCode />
+             </div>
+          </div>
     </div>
   )
 }
