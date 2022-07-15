c0c3f1298c905d0c0fddfc028e18f1c6730e2c33
codewithkevin <oseikelvin709@gmail.com>
2022-07-15 08:32:55 +0000

    update

---
+++ components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="bg-white text-black h-[20%] w-14">
+        hello
+    </div>
+  )
+}
+
+export default Navbar
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
-import {PostCard, PostWidget, Categories} from '../components/index'
+import {PostCard, PostWidget, Categories, Navbar} from '../components/index'
 import {getPosts} from '../services'
+
 
 export default function Home({posts}) {
 
@@ -17,10 +18,13 @@ export default function Home({posts}) {
       </Head>
 
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
+        <div className="lg:col-span-1 lg:flex">
+            <Navbar />
+        </div>
         <div className='lg:col-span-8 col-span-1 lg:grid grid-cols-2 gap-4'>
             {data}
         </div>
-        <div className="lg:col-span-4 col-span-1">
+        <div className="lg:col-span-3 col-span-1">
             <div className="lg:sticky relative top-8">
                <PostWidget />
                <Categories />
