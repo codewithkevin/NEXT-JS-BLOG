7920a066db46b2d71dd855984cb3ec088f785db8
codewithkevin <oseikelvin709@gmail.com>
2022-07-16 20:31:37 +0000

    update

---
+++ components/CategoriesDetail.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CategoriesDetail = () => {
+  return (
+    <div className="bg-red-600 h-full w-full">
+      
+    </div>
+  )
+}
+
+export default CategoriesDetail
--- services/index.js
+++ services/index.js
@@ -149,3 +149,40 @@ export const submitComment = async (obj) => {
     return result.json();
     
 }
+
+export const getCategoryPost = async (slug) => {
+  const query = gql`
+    query GetCategoryPost($slug: String!) {
+      postsConnection(where: {categories_some: {slug: $slug}}) {
+        edges {
+          cursor
+          node {
+            author {
+              bio
+              name
+              id
+              photo {
+                url
+              }
+            }
+            createdAt
+            slug
+            title
+            excerpt
+            featuredImage {
+              url
+            }
+            categories {
+              name
+              slug
+            }
+          }
+        }
+      }
+    }
+  `;
+
+  const result = await request(graphqlAPI, query, { slug });
+
+  return result.postsConnection.edges;
+};
