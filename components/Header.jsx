d1443a01f4805f20e76da0f6ce285a049755fb8a
codewithkevin <oseikelvin709@gmail.com>
2022-07-17 09:31:49 +0000

    setting up a page for categories

--- components/Header.jsx
+++ components/Header.jsx
@@ -23,9 +23,9 @@ const Header = () => {
           </Link>
         </div>
         <div className="hidden md:float-left md:contents">
-          {categories.map((category, index) => (
-            <Link key={index} href={`/category/${category.slug}`}><span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">{category.name}</span></Link>
-          ))}
+        <span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">
+          Categories 
+        </span>
         </div>
       </div>
     </div>
